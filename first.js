912625979944546b949514bcc9c5145b3534b6cb
Tushar <tushar07.11.2002@gmail.com>
2025-01-26 01:29:46 +0530

    sixth commit

--- first.js
+++ first.js
@@ -7,3 +7,10 @@ function updateBalance(){
     balance+=70000000;
 }
 console.log(balance)
+
+
+const ele=document.getElementById('container');
+const card=document.createElement('card');
+const heading=document.createElement('p').innerHTML='this is a card';
+card.appendChild(heading);
+ele.appendChild(card);
